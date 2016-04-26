e3e6827a8a40377a0d4f21b513cadfd729d6ee32
cicciocb <ciccio_cb@yahoo.fr>
2016-04-26 19:48:51 +0200

    version 2.0 alpha 20 cicciocb. Fix the for next
    fix the for next; now it's possible to use variables. example
    
    a = 10
    b = 20
    c= 30
    for i = a to b+c step c/30
    print i
    next i

--- ESP8266Basic/ESP8266Basic.ino
+++ ESP8266Basic/ESP8266Basic.ino
@@ -79,7 +79,7 @@ SoftwareSerial *swSer = NULL;
 //ThingSpeak Stuff
 
 
-PROGMEM const char BasicVersion[] = "ESP Basic 2.0.Alpha cicciocb 19";
+PROGMEM const char BasicVersion[] = "ESP Basic 2.0.Alpha cicciocb 20";
 
 // SPI STUFF
 #include <SPI.h>
